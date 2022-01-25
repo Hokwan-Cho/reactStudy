283b711bc908c629e487b5fcf735b700b70c64d0
hokwan_company <hokwan92@naver.com>
2022-01-25 15:27:59 +0900

    기본적인 CRUD  완성

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Subject from './components/Subject'
 import TOC from './components/TOC'
 import ReadContent from './components/ReadContent'
 import CreateContent from './components/CreateContent'
+import UpdateContent from './components/UpdateContent'
 import Control from './components/Control'
 
 import './App.css';
@@ -26,11 +27,19 @@ class App extends Component{
     }
 
   }
+  getReadContent(){
+    var i = 0; 
+      while(i < this.state.contents.length){
+        var data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id){
+          return data;
+        }
+        i= i+1;
+      }
 
+  }
 
-  render() {
-
-    console.log('App render');
+  getContent(){
     var _title, _desc, _article = null;
 
     if(this.state.mode ==='welcome'){
@@ -38,17 +47,8 @@ class App extends Component{
       _desc= this.state.welcome.desc;
       _article =<ReadContent title ={_title} desc={_desc}></ReadContent>
     }else if(this.state.mode === 'read'){
-      var i = 0; 
-      while(i < this.state.contents.length){
-        var data = this.state.contents[i];
-        if(data.id === this.state.selected_content_id){
-          _title=data.title;
-          _desc=data.desc;
-          break;
-        }
-        i= i+1;
-      }
-      _article =<ReadContent title ={_title} desc={_desc}></ReadContent>
+        var _content = this.getReadContent();
+      _article =<ReadContent title ={_content.title} desc={_content.desc}></ReadContent>
     }else if(this.state.mode === 'create'){
       _article =<CreateContent onSubmit={function(_title,_desc){
   
@@ -68,16 +68,41 @@ class App extends Component{
        var newConetents = Array.from(this.state.contents);
        newConetents.push( {id: this.max_content_id, title: _title, desc:_desc});
        this.setState({
-        contents: newConetents
+        contents: newConetents,
+        mode:'read',
+        selected_content_id : this.max_content_id
       });
 
-       console.log(_title, _desc);
+       console.log("CREATE: " + _title, _desc);
 
       }.bind(this)}></CreateContent>
+    }else if(this.state.mode == 'update'){
+ 
+      var _content = this.getReadContent();
+      _article =<UpdateContent data= {_content} onSubmit={function(_id, _title,_desc){
+       
+       var _contents = Array.from(this.state.contents); 
+        var i = 0 
+        while(i < _contents.length){
+          if(_contents[i].id === _id){
+            _contents[i] = {id: _id, title: _title, desc: _desc};
+            break;
+          }
+          i=i+1;
+        }       
+        this.setState({
+          contents: _contents,
+          mode: "read"
+        });
+      }.bind(this)}></UpdateContent>
     }
+    
+    return _article;
+  }
 
-    console.log("render", this);
+  render() {
 
+    console.log('App render');
     return (
       <div> 
         <Subject 
@@ -93,9 +118,33 @@ class App extends Component{
             
         </TOC>
         <Control onChangeMode={function(_mode){
-            this.setState({"mode":_mode})      
+          if(_mode == 'delete'){
+            if(window.confirm('really?')){
+              var _contents =Array.from(this.state.contents);
+              var i = 0 ;
+
+              console.log(this.state.selcted_content_id);
+              while(i< _contents.length){
+                if(_contents[i].id === this.state.selected_content_id){
+                  _contents.splice(i, 1);          
+                  break;
+                }
+                i = i+1;
+              }
+              this.setState({
+                mode : "welcome",
+                contents: _contents
+              });      
+
+              alert("deleted!");
+            }else{
+              this.steState({
+                mode: _mode
+              });
+            }
+          }
         }.bind(this)}></Control>
-        {_article}
+        {this.getContent()}
       </div> 
     )
   }
